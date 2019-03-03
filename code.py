3ba76b4ad5110efc00f88b7581df71975f992309
Javvaji Sumanth <46456270+JavvajiSumanth@users.noreply.github.com>
2019-03-03 11:15:31 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 09:52:37 2019
+
+@author: Sankar
+"""
+
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+    
+    
